6dc55447d19b22175d2180c1aca144d47840fd8e
Josh <josh.domagala@outlook.com>
2015-05-10 20:31:52 +1000

    viewport

--- view/header.php
+++ view/header.php
@@ -1,8 +1,9 @@
 <html>
 <head>
-<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
-<title><?php echo $title ?></title>
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+	<title><?php echo $title ?></title>
 </head>
 <body>
-<section>
-<h1><?php echo $heading; ?></h1>
+	<section>
+		<h1><?php echo $heading; ?></h1>
